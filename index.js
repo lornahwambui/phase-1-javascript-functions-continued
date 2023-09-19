0f3b0dad6b68951dad5d0a29cca23cc969246fd1
lornahWambuiGitau <lornah.gitau@student.moringaschool.com>
2023-09-19 15:37:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(action="roller-skate"){
+return(`This Saturday, I want to ${action}!`)
+}
+
+
+const mondayWork = function (sentence="go to the office") {
+    return(`This Monday, I will ${sentence}.`)
+}
+
+
+function wrapAdjective(parameter1="*"){
+    return function( parameter="special"){
+        return (`You are ${parameter1}${parameter}${parameter1}!`) 
+}
+
+}
